77d5e95e866733d9aa48a88968674927d6beec33
Adam <a0d1mq@gmail.com>
2023-05-14 19:52:39 +0200

    Create getTasks endpoint

---
+++ Controllers/TaskController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Todo_App.Models;
+
+namespace Todo_App.Controllers {
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TaskController : ControllerBase {
+        private readonly MainDbContext _mainDbContext;
+
+        public TaskController(MainDbContext mainDbContext) {
+            _mainDbContext = mainDbContext;
+        }
+
+        [HttpGet]
+        [Route("GetTasks")]
+        public IActionResult GetTasks() {
+            List<Todo_App.Models.Task> tasks = _mainDbContext.Tasks.ToList();
+            return StatusCode(StatusCodes.Status200OK, tasks);
+        }
+    }
+}
