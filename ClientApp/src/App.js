c350a1cc12edd459688213645d9747e52afd6b93
Adam <a0d1mq@gmail.com>
2023-05-14 19:53:05 +0200

    Test getTasks endpoint

--- ClientApp/src/App.js
+++ ClientApp/src/App.js
@@ -1,5 +1,53 @@
+import { useEffect, useState } from "react";
+
 const App = () => {
-    return(<div></div>)
+
+    // setup useState
+    const [tasks, setTasks] = useState([])
+
+    // call api
+    useEffect(() => {
+        fetch("api/task/GetTasks")
+            .then(response => {
+                return response.json()
+            })
+            .then(responseJson => {
+                setTasks(responseJson)
+            })
+    }, [])
+
+    // create div
+    return (
+        <div className="container">
+            <h1>Tasks</h1>
+            <div className="row">
+                <div className="col-sm-12">
+                    <table className="table table-striped">
+                        <thead>
+                            <tr>
+                                <th>Id</th>
+                                <th>Description</th>
+                                <th>CreatedDate</th>
+                                <th>ListId</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {
+                                tasks.map((item) => (
+                                    <tr key="{item}">
+                                        <td>{item.id}</td>
+                                        <td>{item.description}</td>
+                                        <td>{item.createdDate}</td>
+                                        <td>{item.listId}</td>
+                                    </tr>
+                                ))
+                            }
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default App;
--- ClientApp/src/setupProxy.js
+++ ClientApp/src/setupProxy.js
@@ -5,7 +5,7 @@ const target = env.ASPNETCORE_HTTPS_PORT ? `https://localhost:${env.ASPNETCORE_H
   env.ASPNETCORE_URLS ? env.ASPNETCORE_URLS.split(';')[0] : 'http://localhost:38739';
 
 const context =  [
-  "/weatherforecast",
+  "/api/task",
 ];
 
 module.exports = function(app) {
