869829c7f73094200a8e8ffa31906e6796fa63b5
Adam <a0d1mq@gmail.com>
2023-05-14 19:44:15 +0200

    Setup data access layer (#3)
    * Configure DbContext and models
    * Create db migrations

---
+++ Models/List.cs
@@ -0,0 +1,11 @@
+﻿namespace Todo_App.Models {
+    public class List {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public virtual ICollection<Task> Tasks { get; set; }
+
+        public List() { 
+            this.Tasks = new List<Task>();
+        }
+    }
+}
